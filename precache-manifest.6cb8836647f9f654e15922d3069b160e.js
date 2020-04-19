6eac3dbe409ee724b4db41f2da479ced2b048961
Sohaib Raza <sohaibraza789@gmail.com>
2020-04-19 15:22:38 +0400

    Updates

---
+++ precache-manifest.6cb8836647f9f654e15922d3069b160e.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "978e51b554db00a67e2796661b3526fe",
+    "url": "/klaviyo-dashboard/static/media/100_4.978e51b5.jpg"
+  },
+  {
+    "revision": "97765f6c67f7f78a431c",
+    "url": "/klaviyo-dashboard/static/css/main.a78c52e6.chunk.css"
+  },
+  {
+    "revision": "d8b48fbfc9db4b863932",
+    "url": "/klaviyo-dashboard/static/js/runtime~main.1722d772.js"
+  },
+  {
+    "revision": "42de945c7c1cb65b5941",
+    "url": "/klaviyo-dashboard/static/js/2.64f7eeab.chunk.js"
+  },
+  {
+    "revision": "8efd0a5aac97ea9c70ec",
+    "url": "/klaviyo-dashboard/static/js/3.f9c1a05c.chunk.js"
+  },
+  {
+    "revision": "230cdd5b0007f29c01f4",
+    "url": "/klaviyo-dashboard/static/js/4.4dca7769.chunk.js"
+  },
+  {
+    "revision": "5be7984d74e8d26d2591",
+    "url": "/klaviyo-dashboard/static/js/5.d9897afa.chunk.js"
+  },
+  {
+    "revision": "092cbbd0b7c8292f8b55",
+    "url": "/klaviyo-dashboard/static/js/6.c183e251.chunk.js"
+  },
+  {
+    "revision": "97765f6c67f7f78a431c",
+    "url": "/klaviyo-dashboard/static/js/main.a6d1eedd.chunk.js"
+  },
+  {
+    "revision": "c54eef50718560d1268aa8e92025972d",
+    "url": "/klaviyo-dashboard/static/media/background_1920-2.c54eef50.jpg"
+  },
+  {
+    "revision": "1f7beca5bf4cda1803cd59cd881c372d",
+    "url": "/klaviyo-dashboard/static/media/100_1.1f7beca5.jpg"
+  },
+  {
+    "revision": "82e7c41e073394f808fd382c97e71884",
+    "url": "/klaviyo-dashboard/static/media/100_2.82e7c41e.jpg"
+  },
+  {
+    "revision": "6e25d86d33b97ab231889fd4a38530b4",
+    "url": "/klaviyo-dashboard/static/media/100_3.6e25d86d.jpg"
+  },
+  {
+    "revision": "fd53372585bf0ef377e97ee9dd7ea925",
+    "url": "/klaviyo-dashboard/static/media/100_5.fd533725.jpg"
+  },
+  {
+    "revision": "80d4a4e5af9b6cf3882e4a34c5497542",
+    "url": "/klaviyo-dashboard/static/media/sidebar-4.80d4a4e5.jpg"
+  },
+  {
+    "revision": "b5642d5b5bd83337dded17e55bc0b8cd",
+    "url": "/klaviyo-dashboard/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/KlaviyoDashboard/precache-manifest.96ca33fae6cd5e10dffcb56df772aad7.js"
+  "/klaviyo-dashboard/precache-manifest.6cb8836647f9f654e15922d3069b160e.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/KlaviyoDashboard/index.html", {
+workbox.routing.registerNavigationRoute("/klaviyo-dashboard/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
