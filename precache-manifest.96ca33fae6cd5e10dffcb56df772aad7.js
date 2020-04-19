42c6562c4fa15d22f698250d24ba9549ecfba5b2
Sohaib Raza <sohaibraza789@gmail.com>
2020-04-19 14:38:42 +0400

    Updates

---
+++ precache-manifest.96ca33fae6cd5e10dffcb56df772aad7.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "978e51b554db00a67e2796661b3526fe",
+    "url": "/KlaviyoDashboard/static/media/100_4.978e51b5.jpg"
+  },
+  {
+    "revision": "f2ea20ab9e87f95c6630",
+    "url": "/KlaviyoDashboard/static/css/main.a78c52e6.chunk.css"
+  },
+  {
+    "revision": "b709b302aa644c83e7a8",
+    "url": "/KlaviyoDashboard/static/js/runtime~main.f356cb40.js"
+  },
+  {
+    "revision": "42de945c7c1cb65b5941",
+    "url": "/KlaviyoDashboard/static/js/2.64f7eeab.chunk.js"
+  },
+  {
+    "revision": "8efd0a5aac97ea9c70ec",
+    "url": "/KlaviyoDashboard/static/js/3.f9c1a05c.chunk.js"
+  },
+  {
+    "revision": "230cdd5b0007f29c01f4",
+    "url": "/KlaviyoDashboard/static/js/4.4dca7769.chunk.js"
+  },
+  {
+    "revision": "5be7984d74e8d26d2591",
+    "url": "/KlaviyoDashboard/static/js/5.d9897afa.chunk.js"
+  },
+  {
+    "revision": "092cbbd0b7c8292f8b55",
+    "url": "/KlaviyoDashboard/static/js/6.c183e251.chunk.js"
+  },
+  {
+    "revision": "f2ea20ab9e87f95c6630",
+    "url": "/KlaviyoDashboard/static/js/main.9ad6a30f.chunk.js"
+  },
+  {
+    "revision": "c54eef50718560d1268aa8e92025972d",
+    "url": "/KlaviyoDashboard/static/media/background_1920-2.c54eef50.jpg"
+  },
+  {
+    "revision": "1f7beca5bf4cda1803cd59cd881c372d",
+    "url": "/KlaviyoDashboard/static/media/100_1.1f7beca5.jpg"
+  },
+  {
+    "revision": "82e7c41e073394f808fd382c97e71884",
+    "url": "/KlaviyoDashboard/static/media/100_2.82e7c41e.jpg"
+  },
+  {
+    "revision": "6e25d86d33b97ab231889fd4a38530b4",
+    "url": "/KlaviyoDashboard/static/media/100_3.6e25d86d.jpg"
+  },
+  {
+    "revision": "fd53372585bf0ef377e97ee9dd7ea925",
+    "url": "/KlaviyoDashboard/static/media/100_5.fd533725.jpg"
+  },
+  {
+    "revision": "80d4a4e5af9b6cf3882e4a34c5497542",
+    "url": "/KlaviyoDashboard/static/media/sidebar-4.80d4a4e5.jpg"
+  },
+  {
+    "revision": "39f32641611fd0b2ff5d723e293033a6",
+    "url": "/KlaviyoDashboard/index.html"
+  }
+];
