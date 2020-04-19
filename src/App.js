84da7a2022684b7561928fb00989afe8c0f8532e
Sohaib Raza <sohaibraza789@gmail.com>
2020-04-19 14:16:52 +0400

    Add code

--- src/App.js
+++ src/App.js
@@ -1,33 +1,98 @@
+import { STATE_LOGIN, STATE_SIGNUP } from 'components/AuthForm';
+import GAListener from 'components/GAListener';
+import { EmptyLayout, LayoutRoute, MainLayout } from 'components/Layout';
+import PageSpinner from 'components/PageSpinner';
+import AuthPage from 'pages/AuthPage';
+import React from 'react';
+import componentQueries from 'react-component-queries';
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
+import './styles/reduction.scss';
 
-import React, { Fragment,Component } from "react";
-import AppRoute from "./routes/index";
-import { getItem } from "./utils/localStorage";
-import { ToastContainer } from 'react-toastify';
+// const AlertPage = React.lazy(() => import('pages/AlertPage'));
+const AuthModalPage = React.lazy(() => import('pages/AuthModalPage'));
+// const BadgePage = React.lazy(() => import('pages/BadgePage'));
+// const ButtonGroupPage = React.lazy(() => import('pages/ButtonGroupPage'));
+// const ButtonPage = React.lazy(() => import('pages/ButtonPage'));
+// const CardPage = React.lazy(() => import('pages/CardPage'));
+// const ChartPage = React.lazy(() => import('pages/ChartPage'));
+const DashboardPage = React.lazy(() => import('pages/DashboardPage'));
+const CompaignsPage = React.lazy(() => import('pages/CompaignsPage'));
 
-export const Theme = React.createContext();
 
-const App = () => {
-    const TOKEN = getItem("accessToken");
-    const state = {
-        TOKEN
-    }
-   return( <Fragment>
-        <Theme.Provider value={state}>
-            <AppRoute />
-            <ToastContainer autoClose={8000} />
-        </Theme.Provider>
-    </Fragment>)
+// const DropdownPage = React.lazy(() => import('pages/DropdownPage'));
+// const FormPage = React.lazy(() => import('pages/FormPage'));
+// const InputGroupPage = React.lazy(() => import('pages/InputGroupPage'));
+// const ModalPage = React.lazy(() => import('pages/ModalPage'));
+// const ProgressPage = React.lazy(() => import('pages/ProgressPage'));
+// const TablePage = React.lazy(() => import('pages/TablePage'));
+// const TypographyPage = React.lazy(() => import('pages/TypographyPage'));
+// const WidgetPage = React.lazy(() => import('pages/WidgetPage'));
+
+const getBasename = () => {
+  return `/${process.env.PUBLIC_URL.split('/').pop()}`;
 };
 
-export default App
+class App extends React.Component {
+  render() {
+    return (
+      <BrowserRouter basename={getBasename()}>
+        <GAListener>
+          <Switch>
+            <LayoutRoute
+              exact
+              path="/login"
+              layout={EmptyLayout}
+              component={props => (
+                <AuthPage {...props} authState={STATE_LOGIN} />
+              )}
+            />
+            <LayoutRoute
+              exact
+              path="/signup"
+              layout={EmptyLayout}
+              component={props => (
+                <AuthPage {...props} authState={STATE_SIGNUP} />
+              )}
+            />
 
-// ReactDOM.render(
-//   <BrowserRouter>
-//     <Switch>
-//       <Route path="/admin" render={props => <AdminLayout {...props} />} />
-//       <Route path="/auth" render={props => <AuthLayout {...props} />} />
-//       <Redirect from="/" to="/admin/index" />
-//     </Switch>
-//   </BrowserRouter>,
-//   document.getElementById("root")
-// );
+            <MainLayout breakpoint={this.props.breakpoint}>
+              <React.Suspense fallback={<PageSpinner />}>
+                 <Route exact path="/" component={DashboardPage} />
+                 <Route exact path="/compaigns" component={CompaignsPage} />
+                <Route exact path="/login-modal" component={AuthModalPage} />
+               
+              </React.Suspense>
+            </MainLayout>
+            <Redirect to="/" />
+          </Switch>
+        </GAListener>
+      </BrowserRouter>
+    );
+  }
+}
+
+const query = ({ width }) => {
+  if (width < 575) {
+    return { breakpoint: 'xs' };
+  }
+
+  if (576 < width && width < 767) {
+    return { breakpoint: 'sm' };
+  }
+
+  if (768 < width && width < 991) {
+    return { breakpoint: 'md' };
+  }
+
+  if (992 < width && width < 1199) {
+    return { breakpoint: 'lg' };
+  }
+
+  if (width > 1200) {
+    return { breakpoint: 'xl' };
+  }
+
+  return { breakpoint: 'xs' };
+};
+
+export default componentQueries(query)(App);
--- src/constants/index.js
+++
@@ -1,4 +0,0 @@
-
-
-export const BASE_URL = "https://interest-miner-api.webworks.ninja";
-export const TOKEN = "cdf895267756d954747b63b9cc56e23fa06ecd29";
